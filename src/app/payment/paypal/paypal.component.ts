2cf1befd591d7635f1476dc6e38088981a2e16a4
Raghuraman Kesavan <raghu@canwin.com>
2018-05-31 08:35:09 +0530

    paypal integration completed

---
+++ src/app/payment/paypal/paypal.component.ts
@@ -0,0 +1,58 @@
+import { Component, AfterViewChecked } from '@angular/core';
+
+declare let paypal: any;
+
+@Component({
+  selector: 'paypal-root',
+  templateUrl: './paypal.component.html',
+  styleUrls: ['./paypal.component.css']
+})
+export class PaypalComponent implements AfterViewChecked {
+
+  public didPaypalScriptLoad: boolean = false;
+  public loading: boolean = true;
+
+  public paymentAmount: number = 2000;
+
+  public paypalConfig: any = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+      production: 'xxxxxxxxxx'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.paymentAmount, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        // show success page
+      });
+    }
+  };
+
+  public ngAfterViewChecked(): void {
+    if(!this.didPaypalScriptLoad) {
+      this.loadPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-button');
+        this.loading = false;
+      });
+    }
+  }
+
+  public loadPaypalScript(): Promise<any> {
+    this.didPaypalScriptLoad = true;
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
+  }
+}
