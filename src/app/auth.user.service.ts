2eadcdb940a329974ff6e0a423b3da7363d58154
Siddharth <sarfalesiddharth@gmail.com>
2020-07-28 15:35:40 +0530

    jwt key issue fixed

--- src/app/auth.user.service.ts
+++ src/app/auth.user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthUserService {
-  JWT_LOCALSTORAGE_KEY: 'jwt';
+  JWT_LOCALSTORAGE_KEY = 'jwt';
   user: {
     name: {
       first: string;
@@ -11,35 +11,33 @@ export class AuthUserService {
     email: string;
   };
 
-  constructor() {
-
-  }
+  constructor() {}
 
   setUser = ({ user, token }) => {
     this.user = user;
     window.localStorage.setItem('user', JSON.stringify(this.user));
     this.setToken(token);
-  }
+  };
 
   getUser = () => {
     return JSON.parse(window.localStorage.getItem('user'));
-  }
+  };
 
   unsetUser = () => {
     this.user = null;
     window.localStorage.removeItem('user');
     window.localStorage.removeItem(this.JWT_LOCALSTORAGE_KEY);
-  }
+  };
 
   setToken = (token) => {
     return window.localStorage.setItem(this.JWT_LOCALSTORAGE_KEY, token || '');
-  }
+  };
 
   getToken = (): string => {
     return window.localStorage.getItem(this.JWT_LOCALSTORAGE_KEY) || '';
-  }
+  };
 
   isAuthenticated = (): boolean => {
     return !!this.getToken();
-  }
+  };
 }
